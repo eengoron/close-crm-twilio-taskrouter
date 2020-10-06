740e56d36b94bf133c078be9009d43b0bdf28611
Eric Engoron <eric.engoron@gmail.com>
2020-10-06 13:36:45 -0400

    Rewrite router to use groups and Close's native availability API

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,7 @@
-from flask import Flask, session
-from apscheduler.schedulers.background import BackgroundScheduler
-import logging
-import os
-from .methods import update_close_availability
+from flask import Flask
 
-log = logging.getLogger('apscheduler.executors.default')
-logging.getLogger('apscheduler.executors.default').propagate = False
-log.setLevel(logging.WARNING)
-
-fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
-h = logging.StreamHandler()
-h.setFormatter(fmt)
-log.addHandler(h)
-
-def job1():
-    update_close_availability()
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(job1,"interval", seconds=int(os.environ.get('seconds')))
-scheduler.start()
 app = Flask(__name__)
+from app import routes  # noqa
 
-from app import routes
 if __name__ == '__main__':
     app.run(use_reloader=False, debug=True)
